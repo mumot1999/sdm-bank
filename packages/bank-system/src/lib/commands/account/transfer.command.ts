73344cf67768f3354e9f71c8fa1dd4adb1d234fe
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-29 11:12:26 +0200

    feat(transfer): add transfer command

---
+++ packages/bank-system/src/lib/commands/account/transfer.command.ts
@@ -0,0 +1,25 @@
+import { Account } from '../../domain/account';
+import { AccountInterface } from '../../interfaces/account.interface';
+import { Command } from '../command';
+export class TransferCommand implements Command {
+  private accountA: AccountInterface | undefined;
+  private accountB: AccountInterface | undefined;
+
+  constructor(
+    private idA: string,
+    private idB: string,
+    private amount: number
+  ) {}
+
+  configure(accounts: AccountInterface[]) {
+    this.accountA = accounts.find((account) => account.id === this.idA);
+    this.accountB = accounts.find((account) => account.id === this.idB);
+  }
+
+  execute = () => {
+    if (this.accountA && this.accountB) {
+      this.accountA.subMoney(this.amount);
+      this.accountB.addMoney(this.amount);
+    }
+  };
+}

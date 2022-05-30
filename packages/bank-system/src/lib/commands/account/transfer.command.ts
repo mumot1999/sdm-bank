aee096175678f88a80aa9ed30e4cbd42251f00d5
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-30 12:19:22 +0200

    feat: transfer command gets Transferable

--- packages/bank-system/src/lib/commands/account/transfer.command.ts
+++ packages/bank-system/src/lib/commands/account/transfer.command.ts
@@ -1,25 +1,11 @@
-import { AccountInterface } from '../../interfaces/account.interface';
-import { Identificable } from '../../interfaces/identificable.interface';
+import { Transferable } from '../../interfaces/transferable.interface';
 import { Command } from '../command';
-
-export type TransferAccount = Identificable<{
-  addMoney: (amount: number) => void;
-  subMoney: (amount: number) => void;
-}>;
 export class TransferCommand implements Command {
-  private accountA: TransferAccount | undefined;
-  private accountB: TransferAccount | undefined;
-
   constructor(
-    private idA: string,
-    private idB: string,
+    private accountA: Transferable,
+    private accountB: Transferable,
     private amount: number
   ) {}
-
-  configure(accounts: TransferAccount[]) {
-    this.accountA = accounts.find((account) => account.id === this.idA);
-    this.accountB = accounts.find((account) => account.id === this.idB);
-  }
 
   execute = () => {
     if (this.accountA && this.accountB) {
--- packages/bank-system/src/lib/commands/account/transfer.invoker.ts
+++ packages/bank-system/src/lib/commands/account/transfer.invoker.ts
@@ -1,11 +1,7 @@
-import { TransferCommand, TransferAccount } from './transfer.command';
-import { AccountInterface } from './../../interfaces/account.interface';
+import { TransferCommand } from './transfer.command';
 
 export class TransferInvoker {
-  constructor(private accounts: TransferAccount[]) {}
-
   dispatch(command: TransferCommand): ReturnType<TransferCommand['execute']> {
-    command.configure(this.accounts);
     return command.execute();
   }
 }

af514fb40c465d9237fcd4ee8e6b97e706833d0f
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-30 12:17:44 +0200

    refactor: AccountInterface

--- packages/bank-system/src/lib/interfaces/account.interface.ts
+++ packages/bank-system/src/lib/interfaces/account.interface.ts
@@ -1,6 +1,4 @@
 import { Identificable } from './identificable.interface';
-export type AccountInterface = Identificable<{
-  addMoney: (amount: number) => void;
-  subMoney: (amount: number) => void;
-  getBalance: () => number;
-}>;
+import {Transferable} from './transferable.interface'
+
+export type AccountInterface = Identificable<Transferable>

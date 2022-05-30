70ce2f460231f9aa3eba834130b4cff25f360d3e
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-30 11:57:44 +0200

    fix!: bank-system exports new Account class

--- packages/bank-system/src/index.ts
+++ packages/bank-system/src/index.ts
@@ -1,4 +1,4 @@
-export { Account } from './lib/Account';
+export { Account } from './lib/domain/account';
 export { Loan } from './lib/Loan';
 export { Bank } from './lib/Bank';
 export { DebitAccount } from './lib/DebitAccount';

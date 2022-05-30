37ca09a31e69b93a77dbd9702e0f7d2d14a9c608
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-30 12:19:42 +0200

    refactor(transfer-command): refactor test

--- packages/bank-system/src/lib/commands/account/transfer.spec.ts
+++ packages/bank-system/src/lib/commands/account/transfer.spec.ts
@@ -1,26 +1,25 @@
+import { Transferable } from '../../interfaces/transferable.interface';
 import { AccountInterface } from './../../interfaces/account.interface';
 import { TransferCommand } from './transfer.command';
 import { TransferInvoker } from './transfer.invoker';
 
 describe('transfer command system', () => {
   test('should sub money from accountA and add to accountB', () => {
-    const accountA: AccountInterface = {
+    const accountA: Transferable = {
       addMoney: jest.fn(),
       subMoney: jest.fn(),
       getBalance: () => 0,
-      id: '1111 1111',
     };
 
-    const accountB: AccountInterface = {
+    const accountB: Transferable = {
       addMoney: jest.fn(),
       subMoney: jest.fn(),
       getBalance: () => 0,
-      id: '2222 2222',
     };
 
-    const transferInvoker = new TransferInvoker([accountA, accountB]);
+    const transferInvoker = new TransferInvoker();
 
-    const command = new TransferCommand('1111 1111', '2222 2222', 200);
+    const command = new TransferCommand(accountA, accountB, 200);
     transferInvoker.dispatch(command);
 
     expect(accountA.subMoney).toBeCalledWith(200);

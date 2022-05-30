31dd839b424af248ec4088be3bc06ee78cbb9516
Bartłomiej Mumot <bartlomiej.mumot@gmail.com>
2022-05-30 11:53:06 +0200

    refactor: AccountInterface id Idenficable

--- packages/bank-system/src/lib/interfaces/account.interface.ts
+++ packages/bank-system/src/lib/interfaces/account.interface.ts
@@ -1,6 +1,6 @@
-export type AccountInterface = {
-  id: string;
+import { Identificable } from './identificable.interface';
+export type AccountInterface = Identificable<{
   addMoney: (amount: number) => void;
   subMoney: (amount: number) => void;
   getBalance: () => number;
-};
+}>;
